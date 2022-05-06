43becb08cdd35ee157253d1e55f98f7fd32cb035
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-05-06 17:13:41 +0100

    inheritance tester

--- src/inheritance/InheritanceTester.java
+++ src/inheritance/InheritanceTester.java
@@ -3,6 +3,34 @@ package inheritance;
 public class InheritanceTester {
 
     public static void main(String[] args) {
-        Employee employee = new Employee();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("Square");
+
+        testSquareOverride();
+
+        Mother mom = new Mother();
+        mom.setName("Merlin");
+
+        System.out.println(mom.getName() + " is a " + mom.getGender());
+
+
+
+    }
+
+    public static void testSquareOverride() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setWidth(8);
+        System.out.println(rectangle.calculatePerimeter());
+
+        Square square = new Square();
+        square.setLength(4);
+        square.setWidth(4);
+        System.out.println(square.calculatePerimeter());
+
     }
 }
